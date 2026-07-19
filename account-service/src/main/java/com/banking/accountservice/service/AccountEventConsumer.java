70ebde3df19bdaf187459c9c496061fa11f04fe1
Prakhar Tripathi <gurawliprakhar@gmail.com>
2026-07-19 11:26:50 +0530

    feat: implement Kafka listeners to handle transaction credit and fraud account blocking events

--- account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
+++ account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
@@ -4,21 +4,54 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import java.util.Map;
+import java.math.BigDecimal;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class AccountEventConsumer {
+
+    private final AccountService accountService;
+
     /*
      * consume transection completed event from kafka
+     * Credits reciever account
      * 
      * @param payload
      */
+    @KafkaListener(topics = "transection.completed ")
     public void consumeTransectionCompleted(
             @Payload Map<String, Object> payload) {
         log.info("Received transaction completed event: {}", payload);
+        try {
+            String recieverAccount = (String) payload.get("Reciever account Number");
+            BigDecimal amount = new BigDecimal(payload.get("Amount").toString());
 
+            log.info("Crediting account: {} amount :{}", recieverAccount, amount);
+            accountService.creditBalance(recieverAccount, amount);
+        } catch (Exception e) {
+            log.error("Error crediting account: {}", e.getMessage());
+        }
+    }
+
+    /*
+     * Consume fraud detected event from kafka
+     * it block the account
+     * 
+     * @param payload
+     */
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(@Payload Map<String, Object> payload) {
+        try {
+            String accountNumber = (String) payload.get("Account Number");
+            log.info("Fraud detected for account no: {} ", accountNumber);
+            accountService.blockAccount(accountNumber);
+        } catch (Exception e) {
+            log.error("Error blocking account: {}", e.getMessage());
+        }
     }
 }
