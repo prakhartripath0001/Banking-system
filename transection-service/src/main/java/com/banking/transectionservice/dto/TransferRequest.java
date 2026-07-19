46d161b9ca717f44044ab91e2cd32183e2eac05d
Prakhar Tripathi <gurawliprakhar@gmail.com>
2026-07-19 14:48:30 +0530

    feat: add TransferRequest DTO validation and define TransactionResponse DTO structure

--- transection-service/src/main/java/com/banking/transectionservice/dto/TransferRequest.java
+++ transection-service/src/main/java/com/banking/transectionservice/dto/TransferRequest.java
@@ -1,5 +1,24 @@
 package com.banking.transectionservice.dto;
 
-public class TransferRequest {
+import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferRequest {
+    @NotBlank(message = "sender account number is required")
+    private String senderAccountNumber;
+    @NotBlank(message = "receiver account number is required")
+    private String recieverAccountNumber;
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
+    private BigDecimal amount;
+    private String description;
 }
