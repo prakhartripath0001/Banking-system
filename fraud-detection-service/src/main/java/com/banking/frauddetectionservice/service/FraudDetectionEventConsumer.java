8ff5a063c9eed66e1ecda12b68f81bea876cdc9b
Prakhar Tripathi <gurawliprakhar@gmail.com>
2026-07-20 16:24:41 +0530

    feat: integrate OpenFeign client and update Kafka consumer to initiate fraud detection checks

--- fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionEventConsumer.java
+++ fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionEventConsumer.java
@@ -2,21 +2,35 @@ package com.banking.frauddetectionservice.service;
 
 import java.util.Map;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class FraudDetectionEventConsumer {
+
+    private final FraudDetectionService fraudDetectionService;
+
+    /*
+     * Listens to transaction.intiated
+     * every transaction go through fraud check before completing
+     * 
+     * @param payload
+     */
+    @KafkaListener(topics = "transection-initiated", groupId = "{fraud-detection-group}")
     public void consumeTransectionInitiated(@Payload Map<String, Object> payload) {
         log.info("Received transaction for fraud check: {}", payload.get("transactionId"));
 
         try {
-
+            fraudDetectionService.checkTransaction(payload);
         } catch (Exception e) {
-
+            log.error("Error in fraud detection for transaction {}", payload.get("transactionId"));
+            log.error("Error message: {}", e.getMessage());
         }
     }
 }
--- fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionService.java
+++ fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionService.java
@@ -1,5 +1,19 @@
 package com.banking.frauddetectionservice.service;
 
-public class FraudDetectionService {
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FraudDetectionService {
+    public void checkTransaction(Map<String, String> payload) {
+        String transactionId = (String) payload.get("transactionId");
+        String accountNumber = (String) payload.get("senderAccountNumber");
+        String amount = (String) payload.get("amount");
+
+    }
 }
